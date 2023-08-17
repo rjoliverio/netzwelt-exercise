fbb77cbd835c1dda0ee504cec680ad8e8560a14c
Rogelio John Oliverio <rogeliojohn.oliverio@sun-asterisk.com>
2023-08-17 17:31:21 +0800

    [Server] Implement Login Functionality

--- server/index.js
+++ server/index.js
@@ -1,10 +1,41 @@
-const express = require("express");
-const app = express();
-const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const { default: axios } = require("axios");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
 
-app.get("/", (req, res) => {
-  res.status(200).send("Node Express Server running");
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const request = axios.create({
+  baseURL: process.env.JSON_API_URL,
+  headers: {
+    "X-Requested-With": "XMLHttpRequest",
+  },
+});
+
+app.post("/account/login", async (req, res) => {
+  await request
+    .post("/Account/SignIn", req.body)
+    .then((jsonResponse) => {
+      const accessToken = jwt.sign(req.body, process.env.SECRET_ACCESS_TOKEN);
+      res.send({
+        success: true,
+        data: {
+          user: jsonResponse.data,
+          accessToken: accessToken,
+        },
+        message: "Login Successful.",
+      });
+    })
+    .catch((error) => {
+      res.send({
+        success: false,
+        data: null,
+        message: error.response?.data?.message || error.message,
+      });
+    });
 });
 
 app.listen(process.env.PORT, () => {
